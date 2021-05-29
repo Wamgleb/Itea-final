a940dc74b58c447a9e1eed8a3a5373d2a68eab85
Wamgleb <kalinchukgleb1@gmail.com>
2021-05-29 15:53:18 +0300

    Add API DB

--- app.js
+++ app.js
@@ -14,16 +14,18 @@ app.use(express.urlencoded({ extended: true }));
 mongoose.set("useFindAndModify", false);
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
 console.log("Connected to db!");
-app.listen(3001, () => console.log("Server Up and running"));
+app.listen(3000, () => console.log("Server Up and running"));
 });
 
 app.set("view engine", "ejs");
 
 app.get('/',(req, res) => {
-    res.render('todo.ejs');
+    TodoTask.find({}, (err, tasks) => {
+        res.render("todo.ejs", { todoTasks: tasks });
     });
+});
 
-app.post('/',(req, res) => {
+app.post('/',async(req, res) => {
     const todoTask = new TodoTask({
         content: req.body.content
         });
@@ -34,7 +36,29 @@ app.post('/',(req, res) => {
         res.redirect("/");
         }
     });
-    //console.log(req.body);
+    
+//UPDATE
+app
+    .route("/edit/:id")
+    .get((req, res) => {
+        const id = req.params.id;
+        TodoTask.find({}, (err, tasks) => {
+            res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+        });
+    })
+    .post((req, res) => {
+        const id = req.params.id;
+        TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+            if (err) return res.send(500, err);
+            res.redirect("/");
+        });
+    });
 
-app.listen(3000);
-
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
+    });
+});
